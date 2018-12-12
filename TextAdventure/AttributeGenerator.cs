18fb6139b1a5887dc2d111b7099680378dd75585
Buttstroke <45047969+Buttstroke@users.noreply.github.com>
2018-12-12 20:50:05 +0200

    Added classes for Hero and AttributeGenerator

---
+++ TextAdventure/AttributeGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    public class AttributeGenerator
+    {
+        Random myRandomGenerator = new Random();
+        public Hero Generate(Hero myHero)
+        {
+            myHero.Strength = myRandomGenerator.Next(1, 21);
+            if(myHero.HeroClass.ToLower()=="warrior")
+            {
+                myHero.Strength = myHero.Strength + 3;
+            }
+            else if(myHero.HeroClass.ToLower()=="wizard")
+            {
+                myHero.Strength--;
+            }
+            myHero.Dexterity = myRandomGenerator.Next(1, 21);
+            myHero.Wisdom = myRandomGenerator.Next(1, 21);
+            myHero.Intelligence = myRandomGenerator.Next(1, 21);
+            myHero.Charisma = myRandomGenerator.Next(1, 21);
+            myHero.Constitution = myRandomGenerator.Next(1, 21);
+            myHero.Hitpoints = (myHero.Strength+myHero.Constitution) / 2;
+            return myHero;
+        }
+    }
+}
---
+++ TextAdventure/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextAdventure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your character name?");
+            string name = Console.ReadLine();
+            Hero Buttstroke = new Hero(name);
+            Console.WriteLine("What is your character race?");
+            string race = Console.ReadLine();
+            Buttstroke.Race = race;
+            Console.WriteLine("What is your character class?");
+            string heroclass = Console.ReadLine();
+            Buttstroke.HeroClass = heroclass;
+            AttributeGenerator at = new AttributeGenerator();
+            Buttstroke = at.Generate(Buttstroke);
+
+            Console.WriteLine(Buttstroke.ToString());
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
